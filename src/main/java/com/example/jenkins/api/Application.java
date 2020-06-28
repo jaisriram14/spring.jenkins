546cf750754af81d7f396b56c7d9978198a17067
mallesh <malleshr131@gmail.com>
2020-06-28 18:19:36 +0530

    first commit added newly created project

---
+++ src/main/java/com/example/jenkins/api/Application.java
@@ -0,0 +1,24 @@
+package com.example.jenkins.api;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static org.slf4j.Logger logger=LoggerFactory.getLogger(Application.class);
+	@PostConstruct
+	public void intt() {
+		logger.info("application execution started");
+	}
+
+	public static void main(String[] args) {
+		logger.info("application executed");
+		
+		SpringApplication.run(Application.class, args);
+	}
+
+}
